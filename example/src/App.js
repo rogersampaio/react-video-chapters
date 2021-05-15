34a77f1c2318f0ebf1cec480babbc861f8db9d0f
Roger <rogersampaio@gmail.com>
2021-05-15 19:50:54 +0200

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-video-chapters'
+import 'react-video-chapters/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
